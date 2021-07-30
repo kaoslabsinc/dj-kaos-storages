e4a3042e504c6e39b91d9cb97bf8fcc005feccd7
Keyvan Mosharraf <github@keyvanm.com>
2021-07-30 16:26:38 -0700

    Example models using different storage fields

--- example/sample/models.py
+++ example/sample/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+
+from kaos_storages.fields import PublicFileField, PrivateFileField
+
+
+class DefaultFileUpload(models.Model):
+    file = models.FileField(upload_to='default')
+
+
+class PublicFileUpload(models.Model):
+    file = PublicFileField(upload_to='public')
+
+
+class PrivateFileUpload(models.Model):
+    file = PrivateFileField(upload_to='private')
